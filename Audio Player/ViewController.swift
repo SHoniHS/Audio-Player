e74f0e64ab58ed1f7a4c1fd5c300ea44cbef1b0f
Soni <sherreroa2006@hotmail.com>
2019-01-10 20:09:42 +0100

    Initial audio player playing a Bach Cantata :)

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audiopath = Bundle.main.path(forResource: "CantataBach", ofType: "mp3")
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audiopath!))
+            
+            player.play()
+            
+        } catch {
+            
+            //Process any errors
+            
+        }
+        
     }
 
 
